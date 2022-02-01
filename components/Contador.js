6f4cec319da3a3e5c65e8634cfbb312579108549
Ronald <rdajila@gmail.com>
2022-02-01 17:13:00 +0100

    Component contador

---
+++ components/Contador.js
@@ -0,0 +1,10 @@
+const Contador = () => {
+    return (
+        <div>
+            <h1>Contador: 0</h1>
+            <hr />
+            <button>+ Incrementar</button>
+            <button>- Decrementar</button>
+        </div>
+    );
+}
