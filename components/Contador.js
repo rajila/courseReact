564417326c3e025f8ff137b359adb0ee62d486f2
Ronald <rdajila@gmail.com>
2022-02-02 06:01:27 +0100

    Actions on buttons with arrow functions

--- components/Contador.js
+++ components/Contador.js
@@ -1,10 +1,14 @@
 const Contador = () => {
+    const [contador, setContador] = React.useState(0);
+    const actionIncrementar = () => setContador(contador + 1);
+    const actionDecrementar = () => setContador(contador - 1);
+
     return (
         <div>
-            <h1>Contador: 0</h1>
+            <h1>Contador: {contador}</h1>
             <hr />
-            <button>+ Incrementar</button>
-            <button>- Decrementar</button>
+            <button onClick={actionIncrementar}>+ Incrementar</button>
+            <button onClick={actionDecrementar}>- Decrementar</button>
         </div>
     );
 }
